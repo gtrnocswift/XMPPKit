d65bbe6a725f34b686f3875c8b29caf32d57ea7c
Swift GTRNOC <gtrnoc.swift@gmail.com>
2014-12-05 23:48:47 -0500

    Initial Commit

---
+++ XMPPKit/XMPPKit.h
@@ -0,0 +1,19 @@
+//
+//  XMPPKit.h
+//  XMPPKit
+//
+//  Created by swift on 12/5/14.
+//  Copyright (c) 2014 Georgia Institute of Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XMPPKit.
+FOUNDATION_EXPORT double XMPPKitVersionNumber;
+
+//! Project version string for XMPPKit.
+FOUNDATION_EXPORT const unsigned char XMPPKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XMPPKit/PublicHeader.h>
+
+
